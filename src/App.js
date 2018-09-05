9bdbcf9c0edd38cd94944ebb23e8115136ea66f9
Sara Ebbers <johndoe@example.com>
2018-09-05 16:13:56 -0600

    Change App.js into State/Constructor format

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
+
 import './App.css';
 import key from './APIKey';
 import data from './APIPracticeData';
 
-class App extends Component {
+export default class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      //insert state here and initialize as needed
+    }
+  }
+
+  componentDidMount() {
+    // fetch(enter https)
+      .then(response => response.json())
+      // .then(weather => {
+      //   connect as appropriate
+      // })
+      .catch(error => {
+        console.log(error);
+      })
+  }
+
+
   render() {
     return (
       <div className="App">
@@ -18,6 +39,7 @@ class App extends Component {
       </div>
     );
   }
-}
 
-export default App;
+
+
+}
