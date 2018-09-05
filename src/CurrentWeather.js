abb38a5dd6a6b35a644f814b4e30e07bc87f95c3
Sara Ebbers <johndoe@example.com>
2018-09-05 16:35:56 -0600

    Create CurrentWeather Component file

---
+++ src/CurrentWeather.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+export default class CurrentWeather extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      //add state as needed
+    }
+  }
+
+  render() {
+    //add var here as needed
+
+    return (
+        <li>
+          <p>
+
+          </p>
+        </li>
+      )
+  }
+
+}
