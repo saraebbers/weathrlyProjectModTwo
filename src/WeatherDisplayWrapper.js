ce4a072a1eb30d17239482f6ae52ac67f831a50e
Sara Ebbers <johndoe@example.com>
2018-09-05 16:25:11 -0600

    CreateDisplayWrapper Component file

---
+++ src/WeatherDisplayWrapper.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+// this is a stateless component
+
+import xxx from './...'
+
+export default function WeatherDisplayWrapper(props) {
+  return (
+    <ul>
+      {
+
+      }
+    </ul>
+  );
+
+}
