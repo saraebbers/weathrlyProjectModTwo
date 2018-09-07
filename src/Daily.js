51490202859e9e5430b082269c9878e1eaec767d
Kevin Ryan <kevinpatryan@gmail.com>
2018-09-07 08:46:52 -0600

    Add css for daily

---
+++ src/Daily.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+export default class HourlyWeather extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      //add state as needed
+    }
+  }
+
+  render() {
+    //add var here as needed
+
+    return (
+        <li>
+          <img />
+          <h3>
+            //current temp
+          </h3>
+          <h4>
+            //time
+          </h4>
+        </li>
+      )
+  }
+
+}
