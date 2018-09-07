9517ee2c26e7563a03ed64a984413c9faa64b7c0
Sara Ebbers <johndoe@example.com>
2018-09-07 08:57:24 -0600

    JSX added to Daily.js

--- src/Daily.js
+++ src/Daily.js
@@ -1,33 +1,4 @@
-<<<<<<< HEAD
-import React, { Component } from 'react';
 
-export default class HourlyWeather extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      //add state as needed
-    }
-  }
-
-  render() {
-    //add var here as needed
-
-    return (
-        <li>
-          <img />
-          <h3>
-            //current temp
-          </h3>
-          <h4>
-            //time
-          </h4>
-        </li>
-      )
-  }
-
-}
-=======
 import React from 'react';
 
 import './CurrentWeather';
@@ -36,7 +7,15 @@ import './Daily.css';
 const Daily = (props) => {
   return(
       <div>
-
+        <div>
+          <p>DayOfWeek</p>
+          <p>Date</p>
+        </div>
+        <img />
+        <div>
+          <p>High</p>
+          <p>Low</p>
+        </div>
       </div>
 
 
@@ -44,4 +23,3 @@ const Daily = (props) => {
 }
 
 export default Daily;
->>>>>>> bfba1486a6ea99b46b65335a3cc6024e42dbfb11
