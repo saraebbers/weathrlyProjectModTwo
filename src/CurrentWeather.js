3fd504bd0d783ef939d5ab66d258557a597e3ade
Kevin Ryan <kevinpatryan@gmail.com>
2018-09-08 15:01:14 -0600

    Why is information undefined when it exists

---
+++ src/CurrentWeather.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './CurrentWeather.css';
+
+export const CurrentWeather = (props) => {
+  return (
+    <div>
+      <p>Hi There</p>
+      <p>{currentWeather.location}</p>
+    </div>
+    )
+
+
+} 
