fb102f9177d0767b43f4d0e42ecf4c77b0e93c53
Sara Ebbers <johndoe@example.com>
2018-09-08 13:19:54 -0600

    TenDay added to clean function

--- src/Helper.js
+++ src/Helper.js
@@ -1,7 +1,6 @@
 function clean(dataFromAPI) {
   
   const {display_location, temp_f, observation_time, weather, relative_humidity, icon_url} = dataFromAPI.current_observation;
-     
 
   return {
     currentWeather: {
@@ -16,13 +15,23 @@ function clean(dataFromAPI) {
     hourlyWeather: 
       dataFromAPI.hourly_forecast.slice(0,7).map(eachHour => {
         return {
-          img: eachHour.icon_url,
+          icon: eachHour.icon_url,
           temp: eachHour.temp.english,
           time: eachHour.FCTTIME.civil
         }
-      })
-    // tenDay: [
-    // ]
+      }),
+
+    tenDay: 
+      dataFromAPI.forecast.simpleforecast.forecastday.map(eachDay => {
+        return {
+          dayOfWeek: eachDay.date.weekday,
+          icon: eachDay.icon_url,
+          high: eachDay.high.fahrenheit,
+          low: eachDay.low.fahrenheit,
+          precipitation: eachDay.qpf_allday.in,
+          snow: eachDay.snow_allday.in,
+        }
+      }),
   }
 
 }
